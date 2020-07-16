eeed1be6600db7dff227f9ceb868cdd2bdb66396
David Larsen <davidplarsen@gmail.com>
2020-07-16 17:22:09 -0500

    Barebones routing is up

---
+++ src/components/events/EventList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from "react"
+import EventCard from "./EventCard"
+
+const EventList = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default EventList
